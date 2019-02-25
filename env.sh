04cc933cdd7c6f95f432b68a1d6dd74308e47577
Zhijiang Li <zl256@cornell.edu>
2019-02-25 00:09:35 -0800

    Add comments before install/retrieve

--- env.sh
+++ env.sh
@@ -7,6 +7,7 @@ usage() {
 
     ENV_OPTIONS="$GIT_OPTION $VIMRC_OPTION $ZSHTHEME_OPTION"
 
+    echo "Installs or retrieves some env files"
     echo "./$0 -h|--help"
     echo "./$0 -i|--install $ENV_OPTIONS"
     echo "./$0 -r|--retrieve $ENV_OPTIONS"
@@ -15,14 +16,16 @@ usage() {
 smart_cp() {
     case $ENV_SETUP in
         install)
-            set -x
-            cp "$2" "$PWD/$(basename $1).bak"
+            CURRENT_INSTALL_BAK="$PWD/$(basename $1).install.bak"
+            echo "Saving \"$2\" to \"$CURRENT_INSTALL_BAK\" before overwriting with \"$1\"."
+            cp "$2" "$CURRENT_INSTALL_BAK"
             cp "$1" "$2"
-            set +x
             ;;
         retrieve)
-            set -x
-            cp "$1" "$1.bak"
+            CURRENT_ENV_BAK="$1.bak"
+            echo "Saving \"$1\" to \"$CURRENT_ENV_BAK\" before overwriting with \"$2\"."
+            echo "You have git though, you don't have to be afraid here."
+            cp "$1" "$CURRENT_ENV_BAK"
             cp "$2" "$1"
             set +x
             ;;
